c2eb36186c122a735576eea0e359792e3f149d1d
Sathvik <88944590+User319183@users.noreply.github.com>
2022-02-25 21:04:37 -0500

    Update help.py

--- cogs/help.py
+++ cogs/help.py
@@ -72,44 +72,59 @@ class Help_Commands(commands.Cog):
 
 # Generate a random UUID
 	@slash_command(name="helpme", description="A helpme command")
+	@commands.cooldown(1, 120, commands.BucketType.guild)
 	async def helpme(self, ctx, reason: Option(str, "Specify why you need help.")):
-            embed = discord.Embed(title="Helpme", color=0xD708CC, description=f"```Your help code has been created! Give this code to a moderator or a developer in the Profanity Blocker support server.```")
-            embed.add_field(name = "Code", value = f" ```{uuid.uuid1()}``` ")
-            embed.add_field(name = "Discord Server", value = f" In order for the mods to help you, you need to join the support server: https://discord.gg/ecz2z36gkB ")
-            embed.timestamp = discord.utils.utcnow() 
-            await ctx.respond(embed=embed)
-
-
-            channel = self.bot.get_channel(872858303804350464)
-
-            total_users = len(ctx.guild.members)
-            total_bots = len([member for member in ctx.guild.members if member.bot == True])
-            total_humans = total_users - total_bots
-
-            e = discord.Embed(title="Helpme command used!", color= 0xD708CC)
-            e.add_field(name="Code", value = f" ```{uuid.uuid1()}``` ")
-            e.add_field(name="Reason", value = f" ```{reason}``` ")
-            e.add_field(name="Server Name:", value=ctx.guild.name, inline=False)
-            e.add_field(name="Guild ID", value=ctx.guild.id, inline=False)
-            e.add_field(name="Guild Owner", value=str(ctx.guild.owner), inline=False)
-            e.add_field(name="Guild Users", value="{}".format(total_users))
-            e.add_field(name="Humans", value=total_humans)
-            e.add_field(name="Bots", value=total_bots)
-            try:
-                e.set_thumbnail(url=ctx.guild.icon.url)
-
-            except:
+        
+        
+            if ctx.author.id in badList:
+                return await ctx.respond("You have been banned from doing this command!", ephemeral=True)
+            else:
                 pass
+            
+                if len(reason) > 30:
+                    await ctx.respond("Reason must be lower than 30 characters! Try again in two minutes (120 seconds). ", ephemeral=True) 
+                    
+                else:
+                    
+                    embed = discord.Embed(title="Helpme", color=0xD708CC, description=f"```Your server invite has been created. A staff member or developer will join your server shortly.```")
+                    embed.add_field(name = "Your Code", value = f" ```{uuid.uuid1()}``` ")
+                    embed.add_field(name = "What is the code for?", value = f" In order for us to verify your issue you must send us your code. ")
+                    embed.timestamp = discord.utils.utcnow() 
+                    await ctx.respond(embed=embed)
+                    invite = await ctx.channel.create_invite()
 
-            try:
-                e.add_field(name="Roles", value=", ".join([str(r) for r in ctx.guild.roles]))
 
-            except:
-                pass
+                    channel = self.bot.get_channel(872858303804350464)
 
-            e.timestamp = datetime.utcnow()
+                    total_users = len(ctx.guild.members)
+                    total_bots = len([member for member in ctx.guild.members if member.bot == True])
+                    total_humans = total_users - total_bots
 
-            await channel.send(embed=e)
+                    e = discord.Embed(title="Helpme command used!", color= 0xD708CC)
+                    e.add_field(name="Code", value = f" ```{uuid.uuid1()}``` ")
+                    e.add_field(name="Reason", value = f" ```{reason}``` ")
+                    e.add_field(name="Server Name:", value=ctx.guild.name, inline=False)
+                    e.add_field(name="Guild ID", value=ctx.guild.id, inline=False)
+                    e.add_field(name="Guild Owner", value=str(ctx.guild.owner), inline=False)
+                    e.add_field(name="Guild Users", value="{}".format(total_users))
+                    e.add_field(name="Humans", value=total_humans)
+                    e.add_field(name="Bots", value=total_bots)
+                    e.add_field(name="Invite", value=invite)
+                    try:
+                        e.set_thumbnail(url=ctx.guild.icon.url)
+
+                    except:
+                        pass
+
+                    try:
+                        e.add_field(name="Roles", value=", ".join([str(r) for r in ctx.guild.roles]))
+
+                    except:
+                        pass
+
+                    e.timestamp = datetime.utcnow()
+
+                    await channel.send(embed=e)
 
 
 
